7ac1f390525b1af54c64ee58efe06d040a2aeec5
krish <cbot14365@gmail.com>
2025-06-06 04:56:15 +0530

    fix: cleaned up index.js and removed undefined formData

--- Client/src/index.js
+++ Client/src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import "./index.css";
 
-const response = await fetch(
-  `${process.env.REACT_APP_API_URL}/api/users/login`,
-  {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(formData),
-  }
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
