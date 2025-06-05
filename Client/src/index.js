bfe68708413b8e59bfa9a07c1c7c849c2f7f3c2c
krish <cbot14365@gmail.com>
2025-06-06 04:46:12 +0530

    fix: corrected login API base URL

--- Client/src/index.js
+++ Client/src/index.js
@@ -3,10 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const response = await fetch(
+  `${process.env.REACT_APP_API_URL}/api/users/login`,
+  {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(formData),
+  }
 );
-//jh
